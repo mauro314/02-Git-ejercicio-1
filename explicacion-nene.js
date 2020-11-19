3ac8c47053650f2dd2cb8f89a27aab19d2c44ed0
mauro314 <mauroorellana314@gmail.com>
2020-11-18 21:01:14 -0300

    se respondieron las preguntas en explicacion-nene.js

---
+++ explicacion-nene.js
@@ -0,0 +1,8 @@
+1. En un archivo de texto separado que debes crear, escribe explicaciones de los siguientes conceptos como si se lo estuvieras explicando a un niño de 12 años. Hacer esto te ayudará a descubrir rápidamente cualquier agujero en tu comprensión.
+
+	* Variables es una manera de daerle nombre a una tarear que queremos hacer mas tarde, algo asi como rutaler, ponerle nombre 
+	* Strings podriamos de decir que es espcificar aun mas el nombre de la tarea que asignamos en la variable, y va siempre entre comillas 
+	* Funciones (argumentos, `return`) las funcione es lo principal que debemos saber para poder hacer funcionar variables, string, etc
+	* Declaraciones `if` este tipo de decalaracion signifa que SI suede ésto, pasara tal cosa
+	* Valores booleanos (`true`, `false`) nada mas igual a verdero o falso
+	
